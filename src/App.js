9f4eebdce4a42467d56bc40ac3d8f8e5b60a0800
Thomas Ribeiro <thomashenrique2@gmail.com>
2020-06-16 21:55:18 -0300

    Testes Passando Ok

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 import "./styles.css";
 
+import api from './services/api';
+
 function App() {
+
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+
+      setProjects(response.data);
+
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+
+    const response = await api.post('repositories', {
+
+      title: `Novo Projeto ${Date.now()}`,
+      owner: "Thomas Ribeiro"
+
+    })
+    const project = response.data;
+
+    setProjects([...projects, project])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    
+    await api.delete(`repositories/${id}`);
+
+    setProjects(projects.filter(project => project.id !== id));
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+        {projects.map(project =>
+          <li key={project.id}>
+
+            {project.title}
+
+            <button onClick={() => handleRemoveRepository(project.id)}>
+              Remover
           </button>
-        </li>
+          </li>)}
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
